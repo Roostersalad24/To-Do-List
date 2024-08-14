1eb31a1337b652ed2a601b8cf075c7e0550e3bff
Roostersalad24 <AndrewJohnsonmga@gmail.com>
2024-08-13 23:03:53 -0500

    Initial Commit

---
+++ ToDoList/ToDoTableViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoList
+//
+//  Created by Andrew Johnson on 1/8/24.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    
+    var toDos = [ToDo]()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        if let savedToDos = ToDo.loadToDos() {
+            toDos = savedToDos
+        } else {
+            toDos = ToDo.loadSampleToDos()
+            
+        }
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+   
+    @IBAction func unwind(unwindSegue: UIStoryboardSegue){
+        
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return toDos.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCellIdentifier", for: indexPath)
+        
+        let toDo = toDos[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = toDo.title
+        cell.contentConfiguration = content
+        return cell
+
+    }
+    
+
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+
+
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+           
+            toDos.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }    
+    }
+    
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
